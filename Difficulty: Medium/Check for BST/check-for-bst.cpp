5e615d3033d4365c36eb2dfd3e16122ab1911fa1
Abhay Maddheshiya <abhaymaddheshiya159@gmail.com>
2026-08-28 00:33:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Check for BST/check-for-bst.cpp
@@ -0,0 +1,32 @@
+/* Structure of a Binary Search Tree node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+}; */
+
+class Solution {
+public:
+
+    bool solve(Node* root, long long minVal, long long maxVal) {
+
+        if (!root)
+            return true;
+
+        if (root->data <= minVal || root->data >= maxVal)
+            return false;
+
+        return solve(root->left, minVal, root->data) &&
+               solve(root->right, root->data, maxVal);
+    }
+
+    bool isBST(Node* root) {
+        return solve(root, LLONG_MIN, LLONG_MAX);
+    }
+};
